9582b093da29d123eae4cbc490c00857d40b998f
Michael G. Laird <mgl@mgl.home>
2019-05-28 11:26:55 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,23 @@
-var cardOne = "queen";
+var cards = ["queen", "queen", "king", "king"];
 
-var cardTwo = "king";
+var cardOne = cards[0];
 
-var cardThree = "queen";
+var cardTwo = cards[2];
 
-var cardFour = "king";
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
 
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
